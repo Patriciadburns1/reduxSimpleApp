856f484d84da9fd81b6e583e613b796591ce6d92
dburns1 <patriciadburns@gmail.com>
2018-09-18 15:55:05 -0700

    init commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react'; 
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>React simple starter</div>
-    );
-  }
+const App = function (){
+    return <div> Hi! </div>; 
 }
+
+export default App; 
